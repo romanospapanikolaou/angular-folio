bb9d4c479a9cb204adbbeee901e9891b4c19579d
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-03-28 15:33:13 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './hero/about/about.component';
+import { ProjectsComponent } from './hero/projects/projects.component';
+import { ContactComponent } from './hero/contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, HeroComponent, FooterComponent, AboutComponent, ProjectsComponent, ContactComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
