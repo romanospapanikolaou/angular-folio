0f4cdd1d8dc9d6d4edc41ff7f8befdaf1ca61877
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-01 21:35:33 +0200

    theme icons change

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,4 +5,15 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  iconSrc: string = 'https://img.icons8.com/pulsar-gradient/48/moon-symbol.png';
+
+  changeIcon() {
+    if (this.iconSrc.includes('moon-symbol.png')) {
+      this.iconSrc = 'https://img.icons8.com/office/48/sun.png';
+    } else {
+      this.iconSrc =
+        'https://img.icons8.com/pulsar-gradient/48/moon-symbol.png';
+    }
+  }
+}
