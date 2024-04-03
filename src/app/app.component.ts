bff3d6025db18713a4ed8c21fd0523257dc5c705
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-03 13:32:23 +0200

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  scrollTo(elementId: string): void {
-    const element = document.getElementById(elementId);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    }
-  }
-}
+export class AppComponent {}
