b7c8835bbe32f81678d1629db42b25376fdce35a
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2024-04-12 00:42:40 +0200

    add animation

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  isCurrentPage: boolean = true; // Set this based on whether it's the current page or not
 }
